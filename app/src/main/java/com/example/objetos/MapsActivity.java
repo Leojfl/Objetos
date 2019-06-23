8c6d6fc829dd6fd7ea18c7beccdd3a083d587d0c
Leo <leojfl@outlook.com>
2019-06-23 14:05:37 -0500

    map

---
+++ app/src/main/java/com/example/objetos/MapsActivity.java
@@ -0,0 +1,83 @@
+package com.example.objetos;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private final int REQUEST_ACCESS_FINE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "No", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_ACCESS_FINE
+            );
+            return;
+        }
+        Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        // Add a marker in Sydney and move the camera
+        LatLng latlngMyLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+        Marker fistMarker = mMap.addMarker(new MarkerOptions()
+            .position(latlngMyLocation)
+            .title("Mi ubicación")
+            .snippet("En clase")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        );
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlngMyLocation,16));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+}
--- app/src/main/java/com/example/objetos/MenuActivity.java
+++ app/src/main/java/com/example/objetos/MenuActivity.java
@@ -1,33 +1,75 @@
 package com.example.objetos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn_images, btn_credits, btn_camera, btn_internet, btn_exit;
+    Button btn_images, btn_credits, btn_camera, btn_internet, btn_exit, btn_gps;
+    Intent intentComunication;
 
+    private final int REQUEST_ACCESS_FINE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
         btn_images = findViewById(R.id.btn_images);
+        btn_credits = findViewById(R.id.btn_credits);
+        btn_camera = findViewById(R.id.btn_camera);
+        btn_internet = findViewById(R.id.btn_internet);
+        btn_exit = findViewById(R.id.btn_exit);
+        btn_gps = findViewById(R.id.btn_gps);
+
+        btn_images.setOnClickListener(this);
+        btn_credits.setOnClickListener(this);
+        btn_internet.setOnClickListener(this);
+        btn_exit.setOnClickListener(this);
+        btn_camera.setOnClickListener(this);
+        btn_gps.setOnClickListener(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CAMERA},
+                REQUEST_ACCESS_FINE
+            );
+
+
+        }
+
+
     }
 
     @Override
     public void onClick(View view) {
-
-
+        Toast.makeText(this, "sí preciona", Toast.LENGTH_SHORT).show();
         switch (view.getId()) {
             case R.id.btn_camera:
                 break;
             case R.id.btn_credits:
                 break;
             case R.id.btn_gps:
+                intentComunication = new Intent(this, MapsActivity.class);
+                startActivity(intentComunication);
+
+
                 break;
             case R.id.btn_images:
                 break;
@@ -37,5 +79,28 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        boolean permission = false;
+        for (int x = 0; x < permissions.length; x++) {
+            if (grantResults[x] != PackageManager.PERMISSION_GRANTED) {
+                permission = false;
+                break;
+
+            } else {
+                permission = true;
+            }
+        }
+
+
+        if (!permission) {
+            Toast.makeText(this, "No hay permisos", Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
     }
 }
